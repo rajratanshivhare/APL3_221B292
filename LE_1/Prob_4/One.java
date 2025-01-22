3c83c49c37ad1bd1dbd4a12e1ac3900e9d6e6800
221B292 <221B292@juet.co.in>
2025-01-22 15:25:31 +0530

    OOPs Concepts

---
+++ LE_1/Prob_4/One.java
@@ -0,0 +1,10 @@
+class Mother{
+    Mother(int x){
+        System.out.println("Mother");
+    }
+}
+class One {
+    One(int x) {
+        System.out.println("Parameterized constructor in One: " + x);
+    }
+}
---
+++ LE_1/Prob_4/Two.java
@@ -0,0 +1,6 @@
+class Two extends One {
+    Two(int y) {
+        super(y); 
+        System.out.println("Constructor in Two: " + y);
+    }
+}
